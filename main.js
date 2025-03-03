e7c719a80327db4635f8dcc5581935ea7f8c3868
KillerSosika <skaliush3@gmail.com>
2025-03-03 07:25:03 +0200

    feat: rework Manchester(±1) & 2B1Q(±3) scale (made wider), need final fine-tuning of horizontal/vertical ratio

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-    // інші налаштування
-  })
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
 
-  mainWindow.loadFile(path.join(__dirname, 'scr', 'index.html'))
+  // Завантажуємо index.html
+  win.loadFile(path.join(__dirname, 'scr', 'index.html'));
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
